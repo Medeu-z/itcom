dcdc45d436fec33028517f820811ce53d3557164
sturzhanova <sturzhanova@gmail.com>
2022-03-06 12:42:06 +0600

    added signup and login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type = "text/css" href = "{{url('css/app.css')}}" >
+<title>Login</title>
+
+
+</head>
+<body>
+<div class="login-box">
+<h1>Login</h1>s
+<form>
+	<!--<label>Email/NickName</label>-->
+	<input type="email" placeholder="Email">
+
+	<!--<label>Password</label>-->
+	<input type="password" placeholder="Password">
+	<input type="button" value="Submit">
+</form>
+</div>
+<p class="ph">Not have an account? <a href="index.html">Sign Up Here</a></p>
+
+	</body>
+	</html>
--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -4,10 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type = "text/css" href = "{{url('css/app.css')}}" >
     <title>sign up page</title>
 </head>
 <body>
-    <h1>SIGN UP PAGE </h1>
     
+    
+    <div class="signup-box">
+
+        <h1>Sign Up</h1>
+<form>
+    <!--<label>First Name</label>-->
+    <input type="text" placeholder="First Name">
+
+   <!-- <label>Last Name</label> -->
+
+    <input type="text" placeholder="Last Name">
+     <!--<label>NickName</label>-->
+    <input type="text" placeholder="NickName">
+
+    <!--<label>Email</label>-->
+    <input type="email" placeholder="Email">
+
+    <!--<label>Password</label>-->
+    <input type="password" placeholder="Password">
+
+
+    <!--<label>Confirm Password</label>-->
+    <input type="password" placeholder="Confirm Password">
+    <input type="button" value="Submit">
+</form>
+
+    </div>
+    <p class="ph">Already have an account? <a href="login.html">Login here</a></p>
+
+
 </body>
 </html>
